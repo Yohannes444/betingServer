52209dbc3b0bfdee2ae2336343b24997f1d8a4db
yohannes77 <yohannes.mulat@kegeberewtech.com>
2024-06-28 12:21:06 +0300

    update timer

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string { 
-    const currentTime = new Date().toISOString();
-    return  `${currentTime}` ;
+  getHello(): any {
+    const now = new Date();
+    const currentTime = now;
+
+    const timeDetails = { 
+      time: currentTime, 
+      year: now.getUTCFullYear(), 
+      month: now.getUTCMonth() + 1, // Months are 0-indexed
+      day: now.getUTCDate(), 
+      hour: now.getUTCHours(), 
+      minute: now.getUTCMinutes(), 
+      second: now.getUTCSeconds() 
+    };
+
+    return timeDetails;
   }
 }
