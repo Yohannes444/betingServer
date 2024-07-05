a794459537326ef536d7773e6175a1aa1f45aad2
yohannes444 <yohannesmulat444@gmail.com>
2024-07-05 12:23:12 +0300

    enabel api to get accesd from all places

--- src/main.ts
+++ src/main.ts
@@ -8,29 +8,15 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get('port');
 
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175" // add more as needed
-  ];
-
-
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: true, // Allow all origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  await app.listen(5454, () => {
-    console.log("Server is running on port 5454");
+  await app.listen(port || 5454, () => {
+    console.log(`Server is running on port ${port || 5454}`);
   });
 }
-
 
 bootstrap();
